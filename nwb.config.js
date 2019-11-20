a279b2841d31d018f892dc2984b790212f823ae1
arturgspb <art@realweb.ru>
2019-11-20 23:51:59 +0300

    init

--- nwb.config.js
+++ nwb.config.js
@@ -1,10 +1,14 @@
+var path = require('path');
+
+// https://dev.to/ayabongaqwabi/how-to-build-a-ui-component-library-with-nwb-styled-components-and-storybook-3g3e
+let SRC_DIR = path.resolve('src');
 module.exports = {
   type: 'react-component',
-  webpack: {
-    extractCSS: {
-      filename: '[name].css'
-    }
-  },
+  // webpack: {
+  //   extractCSS: {
+  //     filename: '[name].css'
+  //   }
+  // },
   npm: {
     esModules: true,
     umd: {
---
+++ src/components/Icon/index.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Icon from './index';
+
+export default { title: 'Icon' };
+
+export const small = () => <Icon size='small'>Small</Icon>;
+
+export const medium = () => <Icon size='medium'>Medium</Icon>;
+
+export const large = () => <Icon size='large'>Large</Icon>;
